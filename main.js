5ba5d332b59495d9d3c6e3a835146801efce7348
prajwal896 <prajwaladaki999@gmail.com>
2026-05-10 22:42:58 +0530

    made a project that declutters

--- main.js
+++ main.js
@@ -1,20 +1,52 @@
-const express = require('express')
-const blog = require('./routes/blog')
-const app = express()
-const port = 3000
+import path from 'path';
 
-app.use(express.static("public"))
-app.use('/blog', blog)
-app.post('/', (req, res) => {
-    console.log('Received POST request')
-    res.send('Hello World!2345')
-})
+//let path = 'D:\\java prajwal\\express\\all files\\${path.basename(__filename)}';
 
-app.get('/', (req, res) => {
-    console.log('Received get request')
-    res.send('Hello World!2345')
-})
+import fs from 'fs';
+//console.log(fs);
+//console.log(path);
+let names = fs.readdir('D:\\java prajwal\\express\\all files', (err, s) => {
+    if (err) {
+        console.error(err);
+    } else {
+        for (let i = 0; i < s.length; i++) {
+            //jpg ka codeee
+            if (path.extname(s[i]) == '.jpg') {
+                fs.rename('D:\\java prajwal\\express\\all files\\' + s[i], 'D:\\java prajwal\\express\\jpg\\' + s[i], (err) => {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        console.log("\njpg joined" + s[i]);
+                    }
+                });
+            }
+            //jpg ka codeee
+            else if (path.extname(s[i]) == '.pdf') {
+                fs.rename('D:\\java prajwal\\express\\all files\\' + s[i], 'D:\\java prajwal\\express\\pdf\\' + s[i], (err) => {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        console.log("\npdf joined" + s[i]);
+                    }
+                });
+            }
+            //jpg ka codeee
+            else if (path.extname(s[i]) == '.txt') {
+                fs.rename('D:\\java prajwal\\express\\all files\\' + s[i], 'D:\\java prajwal\\express\\txt\\' + s[i], (err) => {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        console.log("\ntxt joined" + s[i]);
+                    }
+                });
+            }
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+            //jpg ka codeee
+            else {
+                console.log("\nkuch aur hai " + s[i] + "\n");
+            }
+        }
+    }
+});
+
+//console.log(names);
